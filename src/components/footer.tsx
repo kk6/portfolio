052a3eabdef7f0a4ee942b588145ad85a020618e
kk6 <hiro.ashiya@gmail.com>
2021-05-21 11:43:47 +0900

    footerにソーシャルアイコン追加

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,42 @@
 import { Box, Stack, Text, TextProps } from "@chakra-ui/layout"
+import { ButtonGroup, ButtonGroupProps, IconButton } from "@chakra-ui/react"
 import React from "react"
+import { FaGithub, FaTwitter, FaInstagram } from "react-icons/fa"
+import { SiPixiv } from "react-icons/si"
 
 const Copyright = (props: TextProps) => (
   <Text fontSize="sm" {...props}>
     &copy; {new Date().getFullYear()} あしやひろ All rights reserved.
   </Text>
+)
+
+export const SocialMediaLinks = (props: ButtonGroupProps): JSX.Element => (
+  <ButtonGroup variant="ghost" color="gray.600" {...props}>
+    <IconButton
+      as="a"
+      href="https://twitter.com/kk6"
+      aria-label="Twitter"
+      icon={<FaTwitter fontSize="20px" />}
+    />
+    <IconButton
+      as="a"
+      href="https://pixiv.me/ashiyahiro"
+      aria-label="Pixiv"
+      icon={<SiPixiv fontSize="20px" />}
+    />
+    <IconButton
+      as="a"
+      href="https://github.com/kk6"
+      aria-label="GitHub"
+      icon={<FaGithub fontSize="20px" />}
+    />
+    <IconButton
+      as="a"
+      href="https://instagram.com/__kk6__"
+      aria-label="Instagram"
+      icon={<FaInstagram fontSize="20px" />}
+    />
+  </ButtonGroup>
 )
 
 const Footer: React.FC = (): React.ReactElement => (
@@ -16,14 +48,9 @@ const Footer: React.FC = (): React.ReactElement => (
     py="12"
     px={{ base: "4", md: "8" }}
   >
-    <Stack>
-      <Stack
-        direction="row"
-        spacing="4"
-        align="center"
-        justify="space-between"
-      ></Stack>
+    <Stack direction="row" alignItems="center" justify="space-between">
       <Copyright alignSelf={{ base: "center", sm: "start" }} />
+      <SocialMediaLinks />
     </Stack>
   </Box>
 )
