d9bdd49b35db13b9ed288830df3045df7d00c737
kk6 <hiro.ashiya@gmail.com>
2021-05-21 17:19:48 +0900

    フォームの仮デザイン

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,13 +1,37 @@
-import { Container, Text } from "@chakra-ui/layout"
+import { Container, VStack } from "@chakra-ui/layout"
+import {
+  Textarea,
+  Input,
+  Button,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+} from "@chakra-ui/react"
 import { NextPage } from "next"
-
 import Layout from "../components/layout"
 
 const Contact: NextPage = () => {
   return (
     <Layout>
       <Container centerContent>
-        <Text>Coming soon...</Text>
+        <VStack>
+          <FormControl id="email">
+            <FormLabel>メールアドレス</FormLabel>
+            <Input type="email" />
+            <FormHelperText>ヘルプテキスト</FormHelperText>
+          </FormControl>
+          <FormControl id="name">
+            <FormLabel>お名前</FormLabel>
+            <Input type="text" />
+          </FormControl>
+          <FormControl id="body">
+            <FormLabel>お問い合わせ内容</FormLabel>
+            <Textarea placeholder="お問い合わせ内容を入力してください" />
+          </FormControl>
+          <Button colorScheme="teal" variant="outline">
+            Button
+          </Button>
+        </VStack>
       </Container>
     </Layout>
   )
