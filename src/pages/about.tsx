bf5e47c8847640bdbe9cad3c8ad96b08bf091921
kk6 <hiro.ashiya@gmail.com>
2021-05-28 15:42:29 +0900

    :speech_balloon: Fix about page's text.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -31,13 +31,13 @@ const works = {
       url:
         "https://www.animalspedal.jp/products/animals-pedal-custom-illustrated-008-major-overdrive-by-%E3%81%82%E3%81%97%E3%82%84%E3%81%B2%E3%82%8D-%E3%83%9C%E3%83%96%E3%82%AB%E3%83%83%E3%83%88-lightblue",
       title:
-        "Animals Pedal Custom Illustrated Major Overdrive by あしやひろ &quot;ボブカット&ldquo; 筐体表面イラスト",
+        'Animals Pedal Custom Illustrated Major Overdrive by あしやひろ "ボブカット" 筐体表面イラスト',
     },
     {
       url:
         "https://www.animalspedal.jp/products/animals-pedal-custom-illustrated-014-major-overdrive-by-%E3%81%82%E3%81%97%E3%82%84%E3%81%B2%E3%82%8D-alice-faintpink",
       title:
-        "Animals Pedal Custom Illustrated Major Overdrive by あしやひろ &quot;Alice&ldquo; 筐体表面イラスト",
+        'Animals Pedal Custom Illustrated Major Overdrive by あしやひろ "Alice" 筐体表面イラスト',
     },
   ],
 }
@@ -55,15 +55,20 @@ const About: NextPage = () => {
           description,
         }}
       />
-      <Container>
+      <Container maxW="container.lg">
         <VStack spacing={4}>
           <Heading as="h1">About</Heading>
           <Heading as="h2" size="sm" fontWeight="normal">
             あしやひろについて
           </Heading>
-
-          <Avatar src="/icon.webp" alt="あしやひろ" width={128} height={128} />
-
+          <Box>
+            <Avatar
+              src="/icon.webp"
+              alt="あしやひろ"
+              width={128}
+              height={128}
+            />
+          </Box>
           <VStack spacing={4} alignItems="left">
             <Box as="section" padding={1}>
               <Heading as="h1" size="sm">
